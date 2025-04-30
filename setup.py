81c579e42d2e92b46736eff735bb3a4d5a5ea46b
Đặng Dương Minh Nhật <104882522+dangduongminhnhat@users.noreply.github.com>
2025-04-30 23:39:11 +0700

    update new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="circheck",
-    version="0.1.0",
+    version="0.1.1",
     description="Static analysis tool to detect ZKP vulnerabilities in Circom circuits.",
     author="Dang Duong Minh Nhat",
     author_email="dangduongminhnhat2003@gmail.com",
